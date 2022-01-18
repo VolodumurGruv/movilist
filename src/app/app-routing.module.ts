c93656f459767be1742e0cc5ea7800455b985ea5
VolodumurGruv <gru.v@i.ua>
2022-01-18 17:32:46 +0200

    strating template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MovieListComponent },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
