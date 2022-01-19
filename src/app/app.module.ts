355b58ba7ac4f33a18870bcf2ef5ee794bf083ca
VolodumurGruv <gru.v@i.ua>
2022-01-20 01:45:40 +0200

    separate material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
-import { MovieListComponent } from './components/movie-list/movie-list.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { SortComponent } from './components/sort/sort.component';
-import { MatSelectModule } from '@angular/material/select';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AlertComponent } from './components/alert/alert.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { SortComponent } from './components/sort/sort.component';
 
 @NgModule({
-  declarations: [AppComponent, MovieListComponent, SortComponent, AlertComponent],
+  declarations: [
+    AppComponent,
+    MovieListComponent,
+    SortComponent,
+    AlertComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -28,13 +28,7 @@ import { AlertComponent } from './components/alert/alert.component';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    MatTableModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatSelectModule,
-    MatIconModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/material.module.ts
@@ -0,0 +1,30 @@
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSelectModule,
+    MatIconModule,
+  ],
+})
+export class MaterialModule {}
