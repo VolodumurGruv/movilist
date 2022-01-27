368e69a1f9f5201c2b9bbbe8b64c9b5d6ce1abab
VolodumurGruv <gru.v@i.ua>
2022-01-28 00:29:02 +0200

    added node_modules to gitignore

--- server/helpers/getMovie.js
+++ server/helpers/getMovie.js
@@ -3,9 +3,20 @@ const fs = require("fs");
 const csv = require("csv-parser");
 const movies = [];
 
-fs
-  .createReadStream(path.join(__dirname, "../../movies/movies.csv"))
-  .pipe(csv())
-  .on("data", (row) => movies.push(row));
+//fs
+//  .createReadStream(path.join(__dirname, "../../movies/movies.csv"))
+//  .pipe(csv())
+//  .on("data", (row) => movies.push(row));
+
+  fs.readFile(path.join(__dirname, "../../movies/movies.csv"), "utf-8", (err, res) => {
+    if(err) {
+      const error = new Error("File wasn't read")
+      console.error(err);
+      console.error(error)
+      throw err;
+    }
+    movies.push(res.toString().split('\n '));
+    console.log(movies[0]);
+  })
 
   module.exports = movies;
